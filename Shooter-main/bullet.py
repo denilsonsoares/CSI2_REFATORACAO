3a1ca5e385b2bd5a93a16200361e1a3cd0ee024e
Denilson Gustavo <166926902+denilsonsoares@users.noreply.github.com>
2024-06-30 17:29:02 -0300

    bullet update

--- Shooter-main/bullet.py
+++ Shooter-main/bullet.py
@@ -2,33 +2,37 @@
 import pygame
 from settings import SCREEN_WIDTH
 class Bullet(pygame.sprite.Sprite):
-	def __init__(self, x, y, direction, bullet_img, sprite_groups):
-		pygame.sprite.Sprite.__init__(self)
-        self.sprite_groups = sprite_groups
-		self.speed = 10
-		self.image = bullet_img
-		self.rect = self.image.get_rect()
-		self.rect.center = (x, y)
-		self.direction = direction
+    def __init__(self, x, y, direction, bullet_img, sprite_groups):
+        pygame.sprite.Sprite.__init__(self)
+        self.speed = 10
+        self.image = bullet_img
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.direction = direction
+        self.sprite_groups = sprite_groups  # Isso garante que sprite_groups seja uma propriedade da instância
 
-	def update(self, player, world, screen_scroll):
-		#move bullet
-		self.rect.x += (self.direction * self.speed) + screen_scroll
-		#check if bullet has gone off screen
-		if self.rect.right < 0 or self.rect.left > SCREEN_WIDTH:
-			self.kill()
-		#check for collision with level
-		for tile in world.obstacle_list:
-			if tile[1].colliderect(self.rect):
-				self.kill()
+    def update(self, player, world, screen_scroll):
+        # Move bullet
+        self.rect.x += (self.direction * self.speed) + screen_scroll
 
-		#check collision with characters
-		if pygame.sprite.spritecollide(player, self.sprite_groups.bullet_group, False):
-			if player.alive:
-				player.health -= 5
-				self.kill()
-		for enemy in self.sprite_groups.enemy_group:
-			if pygame.sprite.spritecollide(enemy, self.sprite_groups.bullet_group, False):
-				if enemy.alive:
-					enemy.health -= 25
-					self.kill()
+        # Check if bullet has gone off screen
+        if self.rect.right < 0 or self.rect.left > SCREEN_WIDTH:
+            self.kill()
+
+        # Check for collision with level
+        for tile in world.obstacle_list:
+            if tile[1].colliderect(self.rect):
+                self.kill()
+
+        # Check collision with the player
+        if pygame.sprite.collide_rect(self, player):
+            if player.alive:
+                player.health -= 5
+                self.kill()
+
+        # Check collision with each enemy
+        for enemy in self.sprite_groups.enemy_group:
+            if pygame.sprite.collide_rect(self, enemy):
+                if enemy.alive:
+                    enemy.health -= 25
+                    self.kill()
